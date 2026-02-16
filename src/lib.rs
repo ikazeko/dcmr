1423b0777a10f6c858785d23f478cab798c37c25
Ivan Kazeko <ivan.kazeko@avl.com>
2026-02-16 20:47:50 +0100

    update pest grammar

--- src/lib.rs
+++ src/lib.rs
@@ -9,7 +9,7 @@ pub struct DCMParser;
 pub fn parse() {
     let unparsed_file = fs::read_to_string("test.dcm").expect("cannot read file");
 
-    let file = DCMParser::parse(Rule::file, &unparsed_file)
+    let file = DCMParser::parse(Rule::dcm, &unparsed_file)
         .expect("unsuccessful parse") // unwrap the parse result
         .next().unwrap(); // get and unwrap the `file` rule; never fails
 }
